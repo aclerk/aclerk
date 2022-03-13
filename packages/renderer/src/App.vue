61be532fd37405f254bd88f3fb9dfdc128a28bd5
zhaojj11 <zhaojunjie@pyjava.com>
2022-03-13 21:21:35 +0800

    :bricks: build simple framework

--- packages/renderer/src/App.vue
+++ packages/renderer/src/App.vue
@@ -1,31 +1,74 @@
 <template>
-  <img
-    alt="Vue logo"
-    src="../assets/logo.svg"
-    width="300"
-  >
-  <app-navigation />
-  <router-view />
+  <el-container style="height: 100%">
+    <el-aside width="57px" style="background-color: #333333">
+      <router-link to="/" class="ursaa-router">
+        <div class="ursaa-menu">
+          <el-icon :size="36" color="#858585">
+            <house />
+          </el-icon>
+        </div>
+      </router-link>
+      <router-link to="/note" class="ursaa-router">
+        <div class="ursaa-menu">
+          <el-icon :size="36" color="#858585">
+            <notebook />
+          </el-icon>
+        </div>
+      </router-link>
+      <router-link to="/bookkeeping" class="ursaa-router">
+        <div class="ursaa-menu">
+          <el-icon :size="36" color="#858585">
+            <wallet-filled />
+          </el-icon>
+        </div>
+      </router-link>
+      <router-link to="/plan" class="ursaa-router">
+        <div class="ursaa-menu">
+          <el-icon :size="36" color="#858585">
+            <management />
+          </el-icon>
+        </div>
+      </router-link>
+      <router-link to="/setting" class="ursaa-router">
+        <div class="ursaa-menu">
+          <el-icon :size="36" color="#858585">
+            <setting />
+          </el-icon>
+        </div>
+      </router-link>
+    </el-aside>
+    <el-main class="ursaa-container">
+      <router-view v-slot="{ Component }">
+        <keep-alive>
+          <component :is="Component" />
+        </keep-alive>
+      </router-view>
+    </el-main>
+  </el-container>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
-import AppNavigation from '/@/components/AppNavigation.vue';
-export default defineComponent({
-  name: 'App',
-  components: {
-    AppNavigation,
-  },
-});
+
+export default{
+  name: 'App'
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="less">
+.ursaa-menu {
+  height: 56px;
+  width: 56px;
+  display: -webkit-flex;
+  justify-content: center;
+  align-items: center;
+}
+.ursaa-router {
+  color: #858585;
+}
+.ursaa-container {
+  padding: 0 !important;
+}
+.router-link-active .el-icon{
+  color: white;
 }
 </style>
