223ad7de977a7b0c1eea3972969e8164bea57561
zhaojj11 <zhaojunjie@pyjava.com>
2022-03-27 12:06:59 +0800

    :construction: feat: parse and show mermaid
    1. parse and show mermaid
    2. fix commitlint moji

--- packages/renderer/src/utils/mdParser.ts
+++ packages/renderer/src/utils/mdParser.ts
@@ -1,5 +1,41 @@
 import {marked} from 'marked';
 
+class AClerkRender extends marked.Renderer{
+  code(code: string, language: string | undefined, isEscaped: boolean): string {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const lang = (language || '').match(/\S*/)[0];
+    if (this.options.highlight) {
+      const out = this.options.highlight(code, lang);
+      if (out != null && out !== code) {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        isEscaped = true;
+        code = out;
+      }
+    }
+
+    code = code.replace(/\n$/, '') + '\n';
+
+    if(language && language.match(/^mermaid/)){
+      return '<div class="mermaid">' + code + '</div>';
+    }
+
+    if (!lang) {
+      return '<pre><code>'
+        + code
+        + '</code></pre>\n';
+    }
+
+    return '<pre><code class="'
+      + this.options.langPrefix
+      + lang
+      + '">'
+      + code
+      + '</code></pre>\n';
+  }
+}
+
+
 export async function md2html(mdStr: string): Promise<string> {
-  return marked.parse(mdStr);
+  return marked.parse(mdStr, {renderer: new AClerkRender()});
 }
